5e3a51237f430aac5748d4aa3383826588c3b125
That-1-JS_Nerd <itsNathan022@gmail.com>
2022-06-08 06:30:34 +0200

    final commit

--- main.js
+++ main.js
@@ -5,22 +5,19 @@ const overlay = document.querySelector('.overlay');
 const parent = document.querySelector('.grid-container');
 const slider = document.getElementById('rangeSlider');
 const sliderOutput = document.querySelector('.output');
-
 const modalBtn = document.querySelector('.popup');
-const background = document.querySelector('#grid-btn');
 const gridBtn = document.querySelector('#toggle-gridlines');
 const modalExitBtn = document.querySelector('.modal-close');
 const colorBlack = document.querySelector('#select-black');
 const colorRGB = document.querySelector('#select-rgb');
 const eraserBtn = document.querySelector('#eraser');
-const shadeBtn = document.querySelector('#shade')
 
 const parentWidth = 620;
 const parentHeight = 620;
-const coloredCells = [];
+const markedCells = [];
 
-let gridEnabled = false;
 let gridBackground;
+let gridEnabled = false;
 let isRGB = false;
 
 parent.style.width = `${parentWidth}px`;
@@ -39,119 +36,6 @@ function reset() {
     createGrid();
 }
 
-function setGridColor() {
-    if (!parent.childNodes.length) return;
-
-    for (const node of parent.childNodes) {
-        gridBackground = 'rgb(122, 242, 122)';
-        node.style.backgroundColor = gridBackground;
-    }
-}
-
-// =========================================================================
-
-function getShader() {
-    const [R, G, B] = [...getRGB()];
-    const intArr = [];
-    const intRegex = /^[a-z]/;
-    const rgb = `rgb(${R}, ${G}, ${B})`;
-
-    // for (const childNode of parent.childNodes) {
-    //     if (childNode) {
-            
-    //     }
-    // }
-
-    /*
-    const rt = R + (0.25 * (255 - R));
-    const gt = G + (0.25 * (255 - G));
-    const bt = B + (0.25 * (255 - B));
-    */
-
-    for (const node of parent.childNodes) {
-        node.addEventListener('mouseover', (e) => {
-            e.target.removeEventListener('mouseover', randomRGB);
-            console.log(e.target.style.backgroundColor);
-        });
-    }
-}
-
-// =========================================================================
-
-function erase() {
-    for (const node of parent.childNodes) {
-        node.addEventListener('mouseover', (e) => {
-            currentColor = '#fff';
-            e.target.style.backgroundColor = currentColor;
-        });
-    }
-}
-
-function getRGB() {
-    for (;;) {
-        const R = Math.floor(Math.random() * 255);
-        const G = Math.floor(Math.random() * 255);
-        const B = Math.floor(Math.random() * 255);
-        return [R, G, B];
-    }
-}
-
-function randomRGB() {
-    colorRGB.addEventListener('click', () => rgbEnabled = true);
-
-    for (const node of parent.childNodes) {
-        node.addEventListener('mouseover', (e) => {
-            const [R, G, B] = [...getRGB()];
-            currentColor = `rgb(${R}, ${G}, ${B})`;
-            e.target.style.backgroundColor = currentColor;
-        });
-    }
-}
-
-function draw() {
-    for (const node of parent.childNodes) {
-        node.addEventListener('mouseover', (e) => {
-            currentColor = '#000';
-            e.target.style.backgroundColor = currentColor;
-        });
-    }
-}
-
-function createGrid() {
-    const cells = [];
-    const cellCount = slider.value ** 2;
-
-    for (let i = 0; i < cellCount; i++) {
-        const cell = document.createElement('div');
-        if (!gridEnabled) { cell.classList.add('cell-border'); }
-        else { cell.classList.remove('cell-border'); }
-        cell.style.backgroundColor = gridBackground ? gridBackground : '#fff';
-        cells.push(cell);
-    }
-
-    cells.forEach(
-        (cell) => {
-            cell.style.width = `${parentWidth / Math.sqrt(cells.length)}px`;
-            cell.style.height = `${parentWidth / Math.sqrt(cells.length)}px`;
-            parent.appendChild(cell);
-        }
-    );
-}
-
-function mainReset() {
-    createGrid();
-    slider.addEventListener("input", () => {
-        sliderOutput.textContent = slider.value;
-        reset();
-    });
-
-    colorRGB.addEventListener('click', () => randomRGB());
-    eraserBtn.addEventListener('click', () => erase());
-    colorBlack.addEventListener('click', () => draw());
-    shadeBtn.addEventListener('click', () => getShader());
-    background.addEventListener('click', () => setGridColor());
-}
-
 modalBtn.addEventListener('click', () => {
     modal.style.display = 'block';
     overlay.style.display = 'block';
@@ -166,5 +50,96 @@ gridBtn.addEventListener('click', () => {
     gridEnabled = !gridEnabled;
     reset();
 });
+
+function filterRGB(rgb) {
+    if (!rgb) return;
+
+    const rgbArr = rgb.split(',');
+    const intArr = [];
+
+    for (let i = 0; i < rgbArr.length; i++) {
+        const int = rgbArr[i].replace(/[a-z()]/ig, '');
+        intArr.push(parseInt(int));
+    }
+    return intArr;
+}
+
+function createGrid() {
+    const cells = [];
+    const cellCount = slider.value ** 2;
+
+    for (let i = 0; i < cellCount; i++) {
+        const cell = document.createElement('div');
+        cell.style.backgroundColor = 'rgb(255, 255, 255)';
+        if (!gridEnabled) cell.classList.add('cell-border');
+        else cell.classList.remove('cell-border');
+        cells.push(cell);
+    }
+
+    cells.forEach(
+        (cell) => {
+            cell.style.width = `${parentWidth / Math.sqrt(cells.length)}px`;
+            cell.style.height = `${parentWidth / Math.sqrt(cells.length)}px`;
+            parent.appendChild(cell);
+        }
+    );
+}
+
+function main(arg) {
+
+    switch (arg) {
+        case "rgb(0, 0, 0)": // #000
+            for (const node of parent.childNodes) {
+                node.addEventListener('mouseover', (e) => {
+                    e.target.style.backgroundColor = 'rgb(0, 0, 0)';
+                });
+            }
+            break;
+        case "rgb": // RGB
+        for (const node of parent.childNodes) {
+            
+            node.addEventListener('mouseover', (e) => {
+                if (markedCells.indexOf(node) !== -1) console.log("Exists");
+                const R = Math.floor(Math.random() * 255);
+                const G = Math.floor(Math.random() * 255);
+                const B = Math.floor(Math.random() * 255);
+                e.target.style.backgroundColor = `rgb(${R}, ${G}, ${B})`;
+                markedCells.push(e.target);
+            });
+        }
+            break;
+        case "rgb(255, 255, 255)": // Erase
+            for (const node of parent.childNodes) {
+                node.addEventListener('mouseover', (e) => {
+                    e.target.style.backgroundColor = '#fff';
+                });
+            }
+            break;
+        default:
+            return;
+    }
+}
+
+function mainReset() {
+    let param;
+    createGrid();
+    slider.addEventListener("input", () => {
+        sliderOutput.textContent = slider.value;
+        reset();
+    });
+
+    eraserBtn.addEventListener('click', () => {
+        param = "rgb(255, 255, 255)";
+        main(param);
+    });
+    colorBlack.addEventListener('click', () => {
+        param = "rgb(0, 0, 0)";
+        main(param);
+    });
+    colorRGB.addEventListener('click', () => {
+        param = "rgb";
+        main(param);
+    });
+}
 
 mainReset();
